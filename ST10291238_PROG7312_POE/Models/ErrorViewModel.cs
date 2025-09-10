d96a993e66364803b790c5d6effc46e76f31a266
JoshNapier <ST10291238@vcconnect.edu.za>
2025-09-10 12:32:03 +0200

    Add project files.

---
+++ ST10291238_PROG7312_POE/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ST10291238_PROG7312_POE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
