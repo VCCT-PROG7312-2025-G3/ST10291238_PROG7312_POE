dacecf04457dff7a9c7e23d333a001b31ae219be
JoshNapier <ST10291238@vcconnect.edu.za>
2025-09-10 13:18:20 +0200

    Added issue model and issue data type

---
+++ ST10291238_PROG7312_POE/Models/Issue.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ST10291238_PROG7312_POE.Models
+{
+    public class Issue
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime CreatedUtc { get; set; } = DateTime.UtcNow;
+
+        public string Location { get; set; }
+        public IssueCategory Category { get; set; }
+        public string Description { get; set; }
+
+        public string AttachmentPath { get; set; }
+
+        public string Status { get; set; } = "Submitted";
+    }
+}
---
+++ ST10291238_PROG7312_POE/Models/IssueCategory.cs
@@ -0,0 +1,12 @@
+﻿namespace ST10291238_PROG7312_POE.Models
+{
+    public enum IssueCategory
+    {
+        Sanitation = 0,
+        Roads = 1,
+        Utilities = 2,
+        Parks = 3,
+        Safety = 4,
+        Other = 99
+    }
+}
